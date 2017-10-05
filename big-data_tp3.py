fd9ff6f9fa33dbb47a582f8ad77f2beb7799544b
carlossegovia <carlitossegovia1979@gmail.com>
2017-10-05 08:35:01 -0300

    DATASET, SCRIPTS Se agregó el script parametrizado para ingresar el nombre del archivo. También se modificó el script para la realización de los querys.
    Signed-off-by: carlossegovia <carlitossegovia1979@gmail.com>

--- big-data_tp3.py
+++ big-data_tp3.py
@@ -1,12 +1,17 @@
-import csv 
+import csv
 import sys
 import subprocess
-import psycopg2	 
+import psycopg2
 
-# pathDatos = '/home/hduser/Descargas/bigdata/bigdata-setdata.csv'
-# pathNewDatos = '/home/hduser/Descargas/bigdata/data/'
-pathDatos = '/home/ruben/dev/WebSites/input/big-data.csv'
-pathNewDatos = '/home/ruben/dev/WebSites/input/'
+
+fileName = raw_input("Ingrese el nombre del archivo fuente: ")
+pathFileName = '/home/carlitos/Descargas/bigdata/'
+pathDatos = pathFileName + fileName
+
+#pathDatos = '/home/hduser/Descargas/bigdata/bigdata-setdata.csv'
+#pathNewDatos = '/home/hduser/Descargas/bigdata/data/'
+#pathDatos = '/home/carlitos/Descargas/bigdata/bigdata-setdata.csv'
+pathNewDatos = '/home/carlitos/Descargas/bigdata/data/'
 
 try:
     conn = psycopg2.connect("dbname='bigdata' user='postgres' host='localhost' password='postgres'")
@@ -20,40 +25,47 @@ fechas = {}
 result = 0
 
 # Crear Tabla
-querySql = """CREATE TABLE enfermedad (enfermedad TEXT, departamento TEXT, fecha TEXT, contador INTEGER)"""
-cur = conn.cursor();
+querySql = """CREATE TABLE enfermedad_dpto (enfermedad TEXT, departamento TEXT, fecha TEXT, contador INTEGER)"""
+queryDropTable = "DROP TABLE enfermedad_dpto"
+cur = conn.cursor()
+cur.execute(queryDropTable)
 cur.execute(querySql)
 
 f = open(pathDatos)
 reader = csv.reader(f)
 contador = 0
 for row in reader:
-	if contador !=0 :
-		subprocess.call("mkdir " + pathNewDatos+row[0], shell=True)	
-		with open(pathNewDatos+row[0]+'/'+row[0]+'.csv', 'a+') as newCSV :
-			wr = csv.writer(newCSV, quoting=csv.QUOTE_ALL)
-			wr.writerow(row)
+    if contador != 0:
+        subprocess.call("mkdir " + pathNewDatos + row[0], shell=True)
+        with open(pathNewDatos + row[0] + '/' + row[0] + '.csv', 'a+') as newCSV:
+            wr = csv.writer(newCSV, quoting=csv.QUOTE_ALL)
+            wr.writerow(row)
 
-		# Para agregar a la BD relacional
-		try:
-			# Se hace un select por ciudad y departamento 
-			query = "SELECT contador FROM enfermedad_dpto WHERE departamento LIKE %(like)s AND enfermedad LIKE %(like_enf)s"
-			cur.execute(query, dict(like= '%'+row[1]+'%', like_enf='%'+row[4]+'%'))
-			resultados = cur.fetchall()
-			if len(resultados) == 0:
-				cur.execute('INSERT INTO enfermedad_dpto (fecha, departamento, enfermedad, contador) VALUES (%s, %s, %s, %s)', (row[0], row[1], row[4], 1))
-			else:
-				for resultado in resultados:
-					if (resultado[0] != None):
-						result = int(resultado[0]) + 1
-						cur.execute('UPDATE enfermedad_dpto SET contador = %s WHERE departamento = %s AND enfermedad = %s AND fecha = %s', (result, row[1], row[4], row[0]))
-		except psycopg2.Error as e:
-			print e
-			print "I can't update our test database!"
+        # Para agregar a la BD relacional
+        try:
+            # Se hace un select por ciudad y departamento
+            query = "SELECT contador FROM enfermedad_dpto WHERE departamento = %s AND enfermedad = %s AND fecha = %s"
+            cur.execute(query, (row[1], row[4], row[0]))
+            resultados = cur.fetchall()
+            if len(resultados) == 0:
+                cur.execute(
+                    'INSERT INTO enfermedad_dpto (fecha, departamento, enfermedad, contador) VALUES (%s, %s, %s, %s)',
+                    (row[0], row[1], row[4], 1))
+            else:
+                for resultado in resultados:
+                    if (resultado[0] != None):
+                        result = int(resultado[0]) + 1
+                        cur.execute(
+                            'UPDATE enfermedad_dpto SET contador = %s WHERE departamento = %s AND enfermedad = %s AND fecha = %s',
+                            (result, row[1], row[4], row[0]))
+        except psycopg2.Error as e:
+            print e
+            print "I can't update our test database!"
 
-	contador+=1
+    contador += 1
 
-# subprocess.call("hdfs dfs -mkdir input/medicina", shell=True)
-# subprocess.call("hdfs dfs -put data input/medicina", shell=True)
+# subprocess.call("/usr/local/hadoop/bin/hdfs dfs -mkdir /input/medicina", shell=True)
+# subprocess.call("/usr/local/hadoop/bin/hdfs dfs -put /home/hduser/Descargas/bigdata/data/ /input/medicina", shell=True)
 
 f.close()
+
--- cliente.py
+++ cliente.py
@@ -1,11 +1,38 @@
-#encoding: utf-8
+# encoding: utf-8
 import happybase
+import psycopg2
+
+diagnostico = raw_input("Ingrese el diagnostico: ")
+departamento = raw_input("Ingrese el departamento: ")
+fecha = raw_input("Ingrese la fecha: ")
+
+# Example = Anorexia_02-12-2013_Boquerón
+queryHbase = "b'" + diagnostico + "_" + fecha + "_" + departamento + "'"
 
 connection = happybase.Connection('localhost')
 table = connection.table('view')
+row = table.row(queryHbase)
+resultHbase = row[b'cantidad:']
+# print(row[b'cantidad:'])  # prints 'value1'
 
+try:
+    conn = psycopg2.connect("dbname='bigdata' user='postgres' host='localhost' password='postgres'")
+    conn.autocommit = True
+    try:
+        cur = conn.cursor()
+        query = "SELECT contador FROM enfermedad_dpto WHERE departamento = %s AND enfermedad = %s AND fecha = %s"
+        cur.execute(query, (departamento, diagnostico, fecha))
+        resultados = cur.fetchall()
+        if len(resultados) == 0:
+            print "No se encontraron resultados"
+        else:
+            for resultado in resultados:
+                if (resultado[0] != None):
+                    resultPostgres = int(resultado[0])
+    except psycopg2.Error as e:
+        print "Ocurrió un error al realizar la consulta en Postgres"
+        print e
+except:
+    print "I am unable to connect to the database"
 
-
-row = table.row(b'Anorexia_02-12-2013_Boquerón')
-print(row[b'cantidad:'])  # prints 'value1'
-
+print "Cantidad de resultados encontrados: " + int(resultHbase + resultPostgres)
